ea096ed68fb71a4bf9605e877103760a0877beb7
gouthaman <gouthaman87@gmail.com>
2024-04-16 20:30:31 +0100

    first commit

---
+++ restaurentpy/data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+class ReviewData:
+    def __init__(self, path: str) -> None:
+        '''
+        Initializes to read review data
+        
+        Args:
+            path (str): The file path where data saved
+        '''
+        self.path = path
+        
+    def read_review(self, pat: str):
+        '''
+        The function to read files
+        
+        Args:
+            pat (str): The type of file to read (e.g. xlsx)
+        '''
+        if self.pat in ['xlsx']:
+            files = os.listdir(self.path)
+            
+            # print("Reading From:", self.path)
+            # df = pd.read_excel(self.path)
+        return files
+        
